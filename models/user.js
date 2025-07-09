1a12568ac52ac91cdafa5a9d49e707a1ff2e82d9
Angeliniott <angelgarza001a@gmail.com>
2025-07-09 11:03:58 -0600

    Update user.js

--- models/user.js
+++ models/user.js
@@ -9,4 +9,4 @@ const userSchema = new mongoose.Schema({
   fechaIngreso: { type: Date, required: true } // NUEVO
 });
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model('user', userSchema);
